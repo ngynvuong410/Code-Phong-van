d9e6767381abb1f8492416075ccb10324a764807
victim <vuongm910@gmail.com>
2022-08-26 14:22:22 +0700

    make interface login page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,19 @@
+import { Container, Row,Col } from "react-bootstrap";
 import Header from "./componets/Header/Header";
+import Login from "./pages/Login/Login";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Container>
+        <Row>
+          <Col>
+             <Login/>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
