756ba6917ec17afe794f6e91899c0fbd387b109e
victim <vuongm910@gmail.com>
2022-08-28 19:08:16 +0700

    done

--- my-app/src/pages/User/User.js
+++ my-app/src/pages/User/User.js
@@ -1,29 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { Container, Row, Table, Col, Accordion } from 'react-bootstrap'
+import { Pagination } from 'react-bootstrap'
 import './User.scss'
 const Users = [
-  { id: 'a1', fullname: 'CAO QUA', age: '10' },
-  { id: 'a2', fullname: 'TRAN VAN ME', age: '20' },
-  { id: 'c3', fullname: ' VAN AN TOAN', age: '14' },
-  { id: 'd34', fullname: 'TINH ANH', age: '16' },
-  { id: 'a5', fullname: 'LUONG TRI VI ', age: '17' },
-  { id: 'd7', fullname: 'XAM VAN LUOC', age: '19' },
-  { id: 'ff8', fullname: 'Minh Nguyet', age: '19' },
-  { id: 'a9', fullname: 'TRANH PHI HO', age: '32' },
-  { id: 'a10', fullname: 'Thanh CU', age: '14' },
-  { id: 'ff11', fullname: 'VUA VAN VET', age: '29' },
+  { id: 'AK1', fullname: 'CAO QUA', age: '10' },
+  { id: 'BA2', fullname: 'TRAN VAN ME', age: '20' },
+  { id: 'AF3', fullname: ' VAN AN TOAN', age: '14' },
+  { id: 'FA4', fullname: 'TINH ANH', age: '16' },
+  { id: 'RA5', fullname: 'LUONG TRI VI ', age: '17' },
+  { id: 'FA7', fullname: 'XAM VAN LUOC', age: '19' },
+  { id: 'NA8', fullname: 'Minh Nguyet', age: '19' },
+  { id: 'FA9', fullname: 'TRANH PHI HO', age: '32' },
+  { id: 'ZA10', fullname: 'Thanh CU', age: '14' },
+  { id: 'LF0', fullname: 'AN ', age: '18' },
+  { id: 'OP8', fullname: 'Minh kIEN', age: '19' },
+  { id: 'MA9', fullname: 'TRANH  HO', age: '32' },
+  { id: 'PA0', fullname: 'Thanh CU', age: '14' },
+  { id: 'OK10', fullname: ' NIEN', age: '18' },
+
 ]
 const User = () => {
 
   const [listUser, setListUser] = useState([])
   const [listUserToShow, setListUserToShow] = useState([])
-  const [searchID, setSearchId] = useState(1)
   const [tab, settab] = useState(1);
   const [isTag, setisTag] = useState(false);
-  const [idEdit, setidEdit] = useState();
+  const [idEdit, setidEdit] = useState('');
+  const [indexPage, setindexPage] = useState(0);
+  const [searchID, setSearchId] = useState(1)
+  const [strSearch, setstrSearch] = useState('');
+
 
   const navigate = useNavigate();
+  const numPage = Number.parseInt((listUser?.length / 10))
+
 
   useEffect(() => {
     const token = localStorage.getItem('token')
@@ -34,57 +45,11 @@ const User = () => {
       setListUser(Users)
       setListUserToShow(Users)
     } else {
-       navigate("/");
+      navigate("/");
     }
 
   }, [])
 
-  const handelOnSearch = (str) => {
-    let results = []
-    if (searchID == '1') {
-
-      results = listUser.filter((item, idx) => item.id.toLocaleLowerCase().indexOf(str.toLocaleLowerCase()) > -1)
-    } else if (searchID == '2') {
-      results = listUser.filter((item, idx) => item.fullname.toLocaleLowerCase().indexOf(str.toLocaleLowerCase()) > -1)
-
-    } else {
-      results = listUser.filter((item, idx) => item.age.toLocaleLowerCase().indexOf(str.toLocaleLowerCase()) > -1)
-
-    }
-    setListUserToShow(results)
-  }
-  const ShowAllUsers = () => <Row>
-    <div className='search-form'>
-      <form action='' method=''>
-        <div className='search-form__input'>
-          <input type='text' name='search' placeholder='Search...'
-            onChange={(e) => handelOnSearch(e.target.value)}
-          />
-        </div>
-        <select onChange={(e) => setSearchId(e.target.value)} >
-          <option value={1}>ID</option>
-          <option value={2}>FULL NAME</option>
-          <option value={3}>AGE</option>
-        </select>
-      </form>
-    </div>
-    <Table striped>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>ID</th>
-          <th>FULL NAME</th>
-          <th>AGE</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody className='mytable'>
-        {
-          listUserToShow?.slice(0, 20).map((item, idx) => <RenderUser key={idx} item={item} index={idx} />)
-        }
-      </tbody>
-    </Table>
-  </Row>
   const ToolbarLeft = () => {
     return (
       <Accordion className='toolbar'  >
@@ -116,7 +81,17 @@ const User = () => {
       }
       return age;
     }
-
+    //random id
+    function makeid(length) {
+      var result           = '';
+      var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      var charactersLength = characters.length;
+      for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * 
+   charactersLength));
+     }
+     return result;
+  }
     const checkEmptyFiled = (fullname, age) => {
       const messErr = {}
       if (fullname === '' || fullname === undefined) {
@@ -148,7 +123,7 @@ const User = () => {
         } else {
           let results = [];
           results = JSON.parse(localStorage.getItem('listUser'));
-          const obj = { id: Date.now(), fullname: fullname, age: age }
+          const obj = { id: makeid(2).toUpperCase() +Date.now().toString().slice(0,2), fullname: fullname, age: age }
           results.push(obj)
           localStorage.setItem('listUser', JSON.stringify(results));
           setListUserToShow(results)
@@ -227,11 +202,16 @@ const User = () => {
             item = { ...item, age: ageEdit }
           }
           //process array
-          let arr = listUser.filter(el => el.id !== item.id)
-          arr = [...arr, item]
+          let arr = listUser.map(value=>{
+               if(value.id ===id){
+                return item
+               }
+            return value
+          })
           //set Location
-           localStorage.setItem('listUser',JSON.stringify(arr))
-           setListUserToShow(arr)
+          localStorage.setItem('listUser', JSON.stringify(arr))
+          setListUser(arr)
+          setListUserToShow(arr)
         }
       }
     }
@@ -262,15 +242,17 @@ const User = () => {
             <input
               type='date'
               name=''
-              value={ageEdit}
+             
               onChange={(e) => setageEdit(getAge(e.target.value))}
             />
           </td>
           <td className={'pen id_' + item.id}
             style={{ cursor: 'pointer' }}
-            onClick={(e) => handelEditUser(item.id)}
+       
           >
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16"
+                 onClick={(e) => handelEditUser(item.id)}
+            >
               <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
             </svg>
           </td>
@@ -295,7 +277,21 @@ const User = () => {
       </tr>
     )
   }
+  const handelOnSearch = (str) => {
+    setstrSearch(str)
+    let results = []
+    if (searchID == '1') {
 
+      results = listUser.filter((item, idx) => item.id.toLocaleLowerCase().indexOf(str.toLocaleLowerCase()) > -1)
+    } else if (searchID == '2') {
+      results = listUser.filter((item, idx) => item.fullname.toLocaleLowerCase().indexOf(str.toLocaleLowerCase()) > -1)
+
+    } else {
+      results = listUser.filter((item, idx) => item.age.toLocaleLowerCase().indexOf(str.toLocaleLowerCase()) > -1)
+
+    }
+    setListUserToShow(results)
+  }
   return (
     <div className='user-page'>
       <Container>
@@ -305,7 +301,56 @@ const User = () => {
           </Col>
           <Col md={9}>
             {
-              tab == 1 ? <ShowAllUsers /> : <AddNewUser />
+              tab !== 1 ? <AddNewUser /> :
+                (
+                  <Row>
+                    <div className='search-form'>
+                      <form action='' method=''>
+                        <div className='search-form__input'>
+                          <input type='text' name='search' placeholder='Search...'
+                            value={strSearch}
+                            onChange={(e) => handelOnSearch(e.target.value)}
+                          />
+                        </div>
+                        <select onChange={(e) => setSearchId(e.target.value)} >
+                          <option value={1}>ID</option>
+                          <option value={2}>FULL NAME</option>
+                          <option value={3}>AGE</option>
+                        </select>
+                      </form>
+                    </div>
+                    <Table striped>
+                      <thead>
+                        <tr>
+                          <th>#</th>
+                          <th>ID</th>
+                          <th>FULL NAME</th>
+                          <th>AGE</th>
+                          <th></th>
+                        </tr>
+                      </thead>
+                      <tbody className='mytable'> 
+                        {
+                          listUserToShow?.slice(indexPage * 10, (indexPage * 10) + 10).map((item, idx) => <RenderUser key={idx} item={item} index={idx} />)
+                        }
+                      </tbody>
+                    </Table>
+                    <Pagination className='my-pagination'>
+                   
+                      {
+                        Array(numPage + 1).fill().map((value, idx) =>
+                          <Pagination.Item
+                            key={idx}
+                            onClick={() => setindexPage(idx)}
+                          >
+                            {idx + 1}
+                          </Pagination.Item>)
+                      }
+                  
+
+                    </Pagination>
+                  </Row>
+                )
             }
 
           </Col>
